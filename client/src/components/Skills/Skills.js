60670b9b9edff13351c00bb3c0aaa66b5c14b6ad
itkazmi814 <itkazmi814@gmail.com>
2017-11-24 15:31:07 -0600

    High level component layout made. LandingHero WIP

---
+++ client/src/components/Skills/Skills.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Skills = () => (
+	<div>
+		<section class="card">
+			<div class="card-text">
+				<h3> This is the skills section </h3>
+			</div>
+		</section>
+	</div>
+);
+
+export default Skills;
