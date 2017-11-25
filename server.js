c1e9e22189244b58d4f101ca2b83e35c46a1398b
itkazmi814 <itkazmi814@gmail.com>
2017-11-24 22:52:45 -0600

    wip

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const bodyParser = require("body-parser");
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
