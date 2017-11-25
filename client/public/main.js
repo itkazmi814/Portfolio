f9719915d5dfaae35636b112aa60386ecca02d76
itkazmi814 <itkazmi814@gmail.com>
2017-11-25 13:25:53 -0600

    Fixed parallax added. Srolling bug: background img not centered

--- client/public/main.js
+++ client/public/main.js
@@ -2,9 +2,24 @@ console.log("main.js linked");
 
 $(window).scroll(() => {
 	if ($(window).scrollTop() >= $(window).height() * 0.95) {
-		console.log("recognizes scrolling");
+		// console.log("recognizes scrolling");
 		$("#main-navbar").addClass("transparent");
 	} else {
 		$("#main-navbar").removeClass("transparent");
 	}
 });
+
+// $(document).ready(function() {
+// 	$(".parallax-jumbotron").each(function() {
+// 		console.log("hello");
+// 		var $bgobj = $(this); // assigning the object
+// 		$(window).scroll(function() {
+// 			console.log("world");
+// 			var yPos = -$(window).scrollTop() / $bgobj.data("speed");
+// 			// Put together our final background position
+// 			var coords = "30% " + yPos + "px";
+// 			// Move the background
+// 			$bgobj.css({ backgroundPosition: coords });
+// 		});
+// 	});
+// });
--- client/src/components/LandingHero/LandingHero.js
+++ client/src/components/LandingHero/LandingHero.js
@@ -3,8 +3,25 @@ import "./LandingHero.css";
 
 const LandingHero = () => (
 	<section>
-		<div class="jumbotron jumbotron-fluid">
+		{/*}
+		<div class="jumbotron jumbotron-fluid parallax-jumbotron">
 			<div class="jumbotron-contents text-center mt-8">
+				<h1 id="test" class="display-3">
+					Imran Kazmi
+				</h1>
+				<p class="lead">Full Stack Developer</p>
+				<p class="lead">Technology Consultant</p>
+				<p> Github | LinkedIn | Resume </p>
+			</div>
+		</div>
+*/}
+
+		<div
+			class="jumbotron jumbotron-fluid parallax-jumbotron"
+			data-type="background"
+			data-speed="10"
+		>
+			<div class="jumbotron-contents text-center">
 				<h1 id="test" class="display-3">
 					Imran Kazmi
 				</h1>
